b57d0d70098e0cfe7e63ca459b9fc6ec94adcd2a
Laiz <laiz@freeline.br>
2024-06-04 11:13:12 -0300

    hm

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const translateButton = document.getElementById("translateButton");
+//     const languageIndicator = document.getElementById("languageIndicator");
+
+//     let isTranslated = false;
+
+//     translateButton.addEventListener("click", function() {
+//         if (isTranslated) {
+//             // Se já foi traduzido, voltar para PT(BR)
+//             languageIndicator.textContent = "PT(BR)";
+//             isTranslated = false;
+//         } else {
+//             // Se não foi traduzido, mudar para EN(UK)
+//             languageIndicator.textContent = "EN(UK)";
+//             isTranslated = true;
+//         }
+//     });
+// });
